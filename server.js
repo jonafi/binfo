30c337f9dd5c25f51932de092076db0da0500613
unknown <jonafi@gmail.com>
2020-07-20 23:13:12 -0500

    rearranged, fixed async/await issue

--- server.js
+++ server.js
@@ -7,11 +7,28 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+let grabData = async (day)=> {
+    let output = [];
+    let feed = await parser.parseURL(`http://apps.ci.minneapolis.mn.us/CalendarApp/Ex_CalendarRSS.aspx?linkurl=http://www.ci.minneapolis.mn.us/government/calendars.asp&datebook=Garbage%20and%20Recycling%20` + day + `%20Route%20CD&type=rss`);
+    for (let i = 0; i < 5; i++) {
+      output.push(feed.items[i].title)
+    }
+    let data = {output};
+    console.log(data);
+    return data; 
 
 
+};
+ 
 
-app.get("/", (req, res) => {
-  res.send(index.html);
+
+app.get("/rss", async (req, res) => {
+  
+res.json(await grabData("Tuesday"))
+
 });
 
 
